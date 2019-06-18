b87a5cb7bc0e838a6c7df392aa9cf0f10b7270ac
dramforever <dramforever@live.com>
2019-06-19 06:36:02 +0800

    CI: Dump binary cache

---
+++ nixpkgs.nix
@@ -0,0 +1,4 @@
+import (fetchTarball {
+  url = "https://releases.nixos.org/nixpkgs/nixpkgs-19.09pre182717.b58ada326aa/nixexprs.tar.xz";
+  sha256 = "0hk91z5sd2gmmys69673llhxj9af10x2z9gl3yjr42mkfkix2b66";
+})
